cfcf3a8a03779215c0c30339c5ba01a8c919b24e
Marco-Villanueva-21 <159150083+Marco-Villanueva-21@users.noreply.github.com>
2024-04-15 12:28:59 +0000

    removed total when an error occurs

--- handler.php
+++ handler.php
@@ -23,19 +23,19 @@
       $t = $_POST['t'];
     }
 
-    if ($t == 0) {
+    if ($t == 0 && ($s == "large" || $s == "l" || $s == "xlarge" || $s == "xl")) {
       $cost = $cost + 0;
     }
-    if ($t == 1) {
+    if ($t == 1 && ($s == "large" || $s == "l" || $s == "xlarge" || $s == "xl")) {
       $cost = $cost + 1;
     }
-    if ($t == 2) {
+    if ($t == 2 && ($s == "large" || $s == "l" || $s == "xlarge" || $s == "xl")) {
       $cost = $cost + 1.75;
     }
-    if ($t == 3) {
+    if ($t == 3 && ($s == "large" || $s == "l" || $s == "xlarge" || $s == "xl")) {
       $cost = $cost + 2.5;
     }
-    if ($t == 4) {
+    if ($t == 4 && ($s == "large" || $s == "l" || $s == "xlarge" || $s == "xl")) {
       $cost = $cost + 3.35;
     }
 
@@ -52,9 +52,11 @@
     $tax = number_format($cost*0.13, 2);
     $total = number_format($cost+$tax, 2);
 
-    echo "Your subtotal is $cost \n\n";
-    echo "Your tax is $tax \n\n";
-    echo "You total is $total \n\n";
+    if ($s == "large" || $s == "l" || $s == "xlarge" || $s == "xl") {
+      echo "Your subtotal is $cost \n\n";
+      echo "Your tax is $tax \n\n";
+      echo "You total is $total \n\n";
+    }
     ?>
     
   </body>
